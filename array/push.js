a61b45c61734c705874743b01a28bbe32ddb75f1
luis <luisfernandogvv@gmail.com>
2022-12-31 18:58:54 -0300

    feat: Adicionando pseudo-método push

---
+++ array/push.js
@@ -0,0 +1,23 @@
+//Função que simula a lógica do método .reducer, utilizado para adicionar um novo elemento a um array e retorna o novo comprimento do mesmo
+
+Array.prototype.pseudoPush = function ( ...args ){
+
+    const currentArrayLength = this.length;
+
+    for( let i = 0; i < args.length; i++ ){
+
+        this[currentArrayLength + i] = args[i];
+
+    }
+
+    return this.length;
+
+
+}
+
+
+const arr = [1,5];
+
+arr.pseudoPush(6,6);
+
+console.log(arr);
